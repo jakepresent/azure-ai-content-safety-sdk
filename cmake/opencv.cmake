dbe62e46f24afea64ad4a3a5030e88d12012405a
Jaan Akhter <jaanakhter@microsoft.com>
2025-01-03 16:27:18 +0530

    Add opencv module for arm64

--- cmake/opencv.cmake
+++ cmake/opencv.cmake
@@ -16,6 +16,9 @@ set(OPENCV_INSTALL_DIR ${THIRD_PARTY_PATH}/install/opencv)
 set(OPENCV_SHARED_LIB_SOURCE
         "${OPENCV_SOURCE_DIR}/build/x64/vc16/bin/${OPENCV_LIB_NAME}.dll"
         CACHE FILEPATH "shared library source." FORCE)
+set(LOCAL_OPENCV_SHARED_LIB_SOURCE
+        "${CMAKE_SOURCE_DIR}/opencv/windows/arm64/${OPENCV_LIB_NAME}.dll"
+        CACHE FILEPATH "shared library source." FORCE)
 set(OPENCV_SHARED_LIB_DEST
         "${OPENCV_INSTALL_DIR}/lib/${OPENCV_LIB_NAME}.dll"
         CACHE FILEPATH "shared library install target." FORCE)
@@ -23,28 +26,46 @@ set(OPENCV_SHARED_LIB_DEST
 set(OPENCV_STATIC_LIB_SOURCE
         "${OPENCV_SOURCE_DIR}/build/x64/vc16/lib/${OPENCV_LIB_NAME}.lib"
         CACHE FILEPATH "static library source." FORCE)
+set(LOCAL_OPENCV_STATIC_LIB_SOURCE
+        "${CMAKE_SOURCE_DIR}/opencv/windows/arm64/${OPENCV_LIB_NAME}.lib"
+        CACHE FILEPATH "static library source." FORCE)
 set(OPENCV_STATIC_LIB_DEST
         "${OPENCV_INSTALL_DIR}/lib/${OPENCV_LIB_NAME}.lib"
         CACHE FILEPATH "static library target." FORCE)
-
 set(OPENCV_URL
         "https://github.com/opencv/opencv/releases/download/${PACKAGE_VERSION_OPENCV}/opencv-${PACKAGE_VERSION_OPENCV}-windows.exe"
 )
 
-ExternalProject_Add(
-        ${OPENCV_PROJECT}
-        ${EXTERNAL_PROJECT_LOG_ARGS}
-        URL ${OPENCV_URL}
-        PREFIX ${OPENCV_PREFIX_DIR}
-        DOWNLOAD_NO_PROGRESS 1
-        DOWNLOAD_EXTRACT_TIMESTAMP true
-        CONFIGURE_COMMAND ""
-        BUILD_COMMAND ""
-        UPDATE_COMMAND ""
-        INSTALL_COMMAND
-        ${CMAKE_COMMAND} -E copy ${OPENCV_SHARED_LIB_SOURCE} ${OPENCV_SHARED_LIB_DEST} &&
-        ${CMAKE_COMMAND} -E copy ${OPENCV_STATIC_LIB_SOURCE} ${OPENCV_STATIC_LIB_DEST}
-        BUILD_BYPRODUCTS ${OPENCV_SHARED_LIB_DEST})
+string(TOUPPER "${CMAKE_SYSTEM_PROCESSOR}" SYSTEM_PROCESSOR_UPPER)
+if (SYSTEM_PROCESSOR_UPPER STREQUAL "ARM64")
+    ExternalProject_Add(
+            ${OPENCV_PROJECT}
+            ${EXTERNAL_PROJECT_LOG_ARGS}
+            PREFIX ${OPENCV_PREFIX_DIR}
+            DOWNLOAD_COMMAND ""
+            CONFIGURE_COMMAND ""
+            BUILD_COMMAND ""
+            UPDATE_COMMAND ""
+            INSTALL_COMMAND
+            ${CMAKE_COMMAND} -E copy ${LOCAL_OPENCV_SHARED_LIB_SOURCE} ${OPENCV_SHARED_LIB_DEST} &&
+            ${CMAKE_COMMAND} -E copy ${LOCAL_OPENCV_STATIC_LIB_SOURCE} ${OPENCV_STATIC_LIB_DEST}
+            BUILD_BYPRODUCTS ${OPENCV_SHARED_LIB_DEST})
+else()
+    ExternalProject_Add(
+            ${OPENCV_PROJECT}
+            ${EXTERNAL_PROJECT_LOG_ARGS}
+            URL ${OPENCV_URL}
+            PREFIX ${OPENCV_PREFIX_DIR}
+            DOWNLOAD_NO_PROGRESS 1
+            DOWNLOAD_EXTRACT_TIMESTAMP true
+            CONFIGURE_COMMAND ""
+            BUILD_COMMAND ""
+            UPDATE_COMMAND ""
+            INSTALL_COMMAND
+            ${CMAKE_COMMAND} -E copy ${OPENCV_SHARED_LIB_SOURCE} ${OPENCV_SHARED_LIB_DEST} &&
+            ${CMAKE_COMMAND} -E copy ${OPENCV_STATIC_LIB_SOURCE} ${OPENCV_STATIC_LIB_DEST}
+            BUILD_BYPRODUCTS ${OPENCV_SHARED_LIB_DEST})
+endif()
 add_library(opencv STATIC IMPORTED GLOBAL)
 set_property(TARGET opencv PROPERTY IMPORTED_LOCATION ${OPENCV_STATIC_LIB_DEST})
 add_dependencies(opencv ${OPENCV_PROJECT})
